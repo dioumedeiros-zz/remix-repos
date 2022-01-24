cb62825651963a7aa357883838fa12ce97299567
Diou Medeiros <diou.182@gmail.com>
2022-01-23 22:05:07 -0300

    :lipstick: styles: Initialized Chakra-ui components

--- app/features/github/components/GithubContainer.tsx
+++ app/features/github/components/GithubContainer.tsx
@@ -1,5 +1,13 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
 import { Types } from '..';
 
 export const GithubContainer = ({ user }: Types.LoaderData) => {
-  return <img src={user.avatar_url} alt={user.login} />;
+  return (
+    <Box w="100%" m={8}>
+      <Flex alignItems="center" justifyContent="center">
+        <Image src={user.avatar_url} alt={user.login} rounded="full" />
+        <Text fontSize="2xl">{user.login}</Text>
+      </Flex>
+    </Box>
+  );
 };
