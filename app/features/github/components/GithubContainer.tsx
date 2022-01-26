18695e445e8cadc7daceb1a04345cf7e4fea1c8a
Diou Medeiros <diou.182@gmail.com>
2022-01-25 21:41:04 -0300

    :label: chore: Github types adjustments

--- app/features/github/components/GithubContainer.tsx
+++ app/features/github/components/GithubContainer.tsx
@@ -1,9 +1,11 @@
 import { Box, Flex, Image, Text } from '@chakra-ui/react';
-import { Types } from '..';
+import { Header } from '~/components/Header';
+import { LoaderData } from '../types';
 
-export const GithubContainer = ({ user }: Types.LoaderData) => {
+export const GithubContainer = ({ user }: LoaderData) => {
   return (
-    <Box w="100%" m={8}>
+    <Box w="100%">
+      <Header user={user} />
       <Flex alignItems="center" justifyContent="center">
         <Image src={user.avatar_url} alt={user.login} rounded="full" />
         <Text fontSize="2xl">{user.login}</Text>
